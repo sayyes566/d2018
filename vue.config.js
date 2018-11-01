f1a61e26cafe003cd3950e73cb391288a073f7de
Kristen <sayyes566@gmail.com>
2018-11-01 15:27:12 +0000

    add jquery

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
+const webpack = require('webpack');
+
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/production-sub-path/'
-    : '/'
+    : '/',
+  chainWebpack: config => {
+        config
+            .plugin('provide')
+            .use(webpack.ProvidePlugin, [{
+                $: 'jquery',
+                jquery: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery'
+            }]);
+    },
 }
